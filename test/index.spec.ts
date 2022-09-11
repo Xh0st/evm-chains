3e885f57818acd0ce1598205a05b0846b0562636
Xh0st <and.andrei@outlook.com>
2022-09-12 01:10:21 +0300

    Update index.spec.ts

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -5,13 +5,8 @@ describe('evm-chains', () => {
     const chain = evmChains.getChain(1);
     expect(chain.name).toEqual('Ethereum Mainnet');
     expect(chain.chainId).toEqual(1);
-    expect(chain.shortName).toEqual('eth');
-    expect(chain.chain).toEqual('ETH');
-    expect(chain.network).toEqual('mainnet');
-    expect(chain.networkId).toEqual(1);
     expect(chain.nativeCurrency.name).toEqual('Ether');
     expect(chain.nativeCurrency.symbol).toEqual('ETH');
     expect(chain.nativeCurrency.decimals).toEqual(18);
-    expect(chain.infoURL).toEqual('https://ethereum.org');
   });
 });
